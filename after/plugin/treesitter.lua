f0cd8d69fe73bd0a9f46dd4d9195ced9a40f7b7c
David Odohi <odohi.david@gmail.com>
2024-02-29 21:35:33 +0000

    configure tsx

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,9 +1,14 @@
 require"nvim-treesitter.configs".setup {
-  ensure_installed = { "go", "lua", "vim", "vimdoc", "tsx", "css", "rust", "typescript", "javascript", "html" },
+  ensure_installed = { "go", "lua", "json", "vim", "vimdoc", "tsx", "css", "rust", "typescript", "javascript", "html" },
   sync_install = false,
-  highlight = { 
+  highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
   indent = { enable = true },
+  autotag = { enable = true }
 }
+
+-- configure tsx setup
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
