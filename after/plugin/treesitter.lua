076a1aab36f5bde914330d74b2e2e53ec383dc95
David Odohi <odohi.david@gmail.com>
2024-04-16 10:42:45 +0100

    add python support

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,9 @@
 require"nvim-treesitter.configs".setup {
-  ensure_installed = { "go", "lua", "json", "vim", "vimdoc", "tsx", "css", "rust", "typescript", "javascript", "html" },
+  ensure_installed = { 
+    "go", "lua", "json", "vim", "vimdoc",
+    "tsx", "css", "rust", "typescript",
+    "javascript", "html", "python"
+  },
   sync_install = false,
   highlight = {
     enable = true,
